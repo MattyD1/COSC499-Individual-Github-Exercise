ec33b0efe26a1a0ac23610eca013b00d09389a75
Matt Dunn <mattdunn0816@gmail.com>
2022-09-13 20:56:08 -0700

    Added a test array and applied first split

--- mergeSort.py
+++ mergeSort.py
@@ -1,1 +1,9 @@
-print("Merge Sort")
+# Merge Sort Algorithm
+
+testArray = [1, 4, 2, 6, 10, 12, 44]
+
+# Integer divide because the array length is odd
+mid = len(testArray)//2
+# Split the array into 2
+b = testArray[:mid]
+e = testArray[mid:]
