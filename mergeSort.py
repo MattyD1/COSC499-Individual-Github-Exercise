cc66ad708f202e7c2c83b471b51d51da007f85f1
Matt Dunn <mattdunn0816@gmail.com>
2022-09-13 21:07:13 -0700

    Added Merge Sort Algorithm

--- mergeSort.py
+++ mergeSort.py
@@ -1,9 +1,40 @@
 # Merge Sort Algorithm
 
-testArray = [1, 4, 2, 6, 10, 12, 44]
+def mergeSort(array):
+  if len(array) > 1:
+    
+    # Integer divide because the array length is odd
+    mid = len(array)//2
+    # Split the array into 2
+    b = array[:mid]
+    e = array[mid:]
 
-# Integer divide because the array length is odd
-mid = len(testArray)//2
-# Split the array into 2
-b = testArray[:mid]
-e = testArray[mid:]
+    # Merge sort is recursive thus we call it again for each half
+    mergeSort(b)
+    mergeSort(e)
+
+    i = j = l = 0
+
+    while i < len(b) and j < len(e):
+      if b[i] < e[j]:
+        array[l] = b[i]
+        i+=1
+      else: 
+        array[l] = e[j]
+        j+=1
+      
+      l+=1
+
+    while i < len(b):
+      array[l] = b[i]
+      i+=1
+      l+=1
+
+    while j < len(e):
+      array[l] = e[j]
+      j+=1
+      l+=1
+
+if __name__ == '__main__':
+  testArray = [1, 4, 2, 6, 10, 12, 44]
+  mergeSort(testArray)
